6b18a6347b0047741e3e7c4c2167a61dd787c498
panchitofranka-cell <panchitofrank.a@gmail.com>
2026-09-03 22:07:54 +0200

    MAJ 3 sept 2026 - recap + import Excel en haut, saisie par caissier, totaux mensuels, remise a zero chaque mois

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Cache simple : la page marche hors ligne une fois ouverte une première fois. */
-var CACHE = 'pilotage-v3';
+var CACHE = "pilotage-v5";
 var FILES = ['./', './index.html', './manifest.json',
              './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
