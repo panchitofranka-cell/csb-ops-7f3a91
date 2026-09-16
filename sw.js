b19d25c1137082be45fd11976ada8001e6562199
panchitofranka-cell <panchitofrank.a@gmail.com>
2026-09-16 14:56:28 +0200

    Integrar boton Ruta del dia en la app (tareas) + sw v15

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Cache simple : la page marche hors ligne une fois ouverte une première fois. */
-var CACHE = "pilotage-v14";
+var CACHE = "pilotage-v15";
 var FILES = ['./', './index.html', './manifest.json',
              './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
